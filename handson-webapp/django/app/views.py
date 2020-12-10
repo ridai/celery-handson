b2d23594de6fc55d4067426dfe3c7e6cb3bb4b14
michihiro naito <>
2020-12-10 10:46:59 +0900

    fix

--- handson-webapp/django/app/views.py
+++ handson-webapp/django/app/views.py
@@ -19,8 +19,6 @@ class Base(generic.TemplateView):
 
     def get_context_data(self, **kwargs):
         ctx = super().get_context_data(**kwargs)
-        ctx["body"] = "sample"
-
         return ctx
 
 
